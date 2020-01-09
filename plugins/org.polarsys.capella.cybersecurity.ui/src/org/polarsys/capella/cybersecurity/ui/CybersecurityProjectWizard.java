35a871b76afce0d44851f8cee22b60842b2cb3b3
Philippe DUL <philippe.dul@thalesgroup.com>
2020-01-09 13:23:25 +0100

    [555020] Update New project wizard text
    Bug: 555020
    Change-Id: Ifa3ed06c04da27fc76bf719eb6ced3c1671d0355
    Signed-off-by: Philippe DUL <philippe.dul@thalesgroup.com>

--- plugins/org.polarsys.capella.cybersecurity.ui/src/org/polarsys/capella/cybersecurity/ui/CybersecurityProjectWizard.java
+++ plugins/org.polarsys.capella.cybersecurity.ui/src/org/polarsys/capella/cybersecurity/ui/CybersecurityProjectWizard.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2019 THALES GLOBAL SERVICES.
+ * Copyright (c) 2020 THALES GLOBAL SERVICES.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -30,9 +30,12 @@ public class CybersecurityProjectWizard extends NewProjectWizard {
 
   @Override
   protected WizardNewProjectCreationPage createLocalProjectDescriptionPage() {
-    return super.createLocalProjectDescriptionPage();
+    WizardNewProjectCreationPage page = super.createLocalProjectDescriptionPage();
+    page.setDescription(Messages.CybersecurityProjectWizard_description);
+    page.setTitle(Messages.CybersecurityProjectWizard_title);
+    return page;
   }
-
+  
   @Override
   protected SessionCreationHelper createSessionCreationHelper() {
     return new ProjectSessionCreationHelper(modelPage.isEpbsSelected(), modelPage.isOpaSelected(),
