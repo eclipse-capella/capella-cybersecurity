2c301a56eab7e0e0a91920cadd3a36ff4e972866
Tu Ton <minhtutonthat@gmail.com>
2020-01-14 16:48:34 +0100

    [568028][Cybersecurity VP] Activity Explorer issue when updating activities with a VP contribution
    Bug: 568028
    Change-Id: Iadef2312e11762e2f553442c1327de3f2e32e31b
    Signed-off-by: Tu Ton <minhtutonthat@gmail.com>

--- plugins/org.polarsys.capella.cybersecurity.ui/src/org/polarsys/capella/cybersecurity/ui/activity/predicate/IsViewpointActivatedPredicate.java
+++ plugins/org.polarsys.capella.cybersecurity.ui/src/org/polarsys/capella/cybersecurity/ui/activity/predicate/IsViewpointActivatedPredicate.java
@@ -10,39 +10,26 @@
  *******************************************************************************/
 package org.polarsys.capella.cybersecurity.ui.activity.predicate;
 
-import org.eclipse.amalgam.explorer.activity.ui.api.editor.ActivityExplorerEditor;
+import org.eclipse.amalgam.explorer.activity.ui.api.editor.activities.ExplorerActivity;
 import org.eclipse.amalgam.explorer.activity.ui.api.editor.input.ActivityExplorerEditorInput;
 import org.eclipse.amalgam.explorer.activity.ui.api.editor.predicates.IPredicate;
 import org.eclipse.sirius.business.api.session.Session;
-import org.eclipse.ui.IEditorPart;
-import org.eclipse.ui.PlatformUI;
 import org.polarsys.capella.core.data.capellamodeller.Project;
 import org.polarsys.capella.core.sirius.ui.helper.SessionHelper;
 import org.polarsys.capella.cybersecurity.CyberSecurityViewpointHelper;
 
 public class IsViewpointActivatedPredicate implements IPredicate {
-
   @Override
-  public boolean isOk() {
-    IEditorPart activeEditor = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor();
-
-    if (activeEditor instanceof ActivityExplorerEditor) {
-      ActivityExplorerEditor activityExplorerEditor = (ActivityExplorerEditor) activeEditor;
-      ActivityExplorerEditorInput editorInput = activityExplorerEditor.getEditorInput();
-
-      if (editorInput != null) {
-        Object adapter = editorInput.getAdapter(Session.class);
-
-        if (adapter instanceof Session) {
-          Session session = (Session) adapter;
-          Project capellaProject = SessionHelper.getCapellaProject(session);
-
-          return CyberSecurityViewpointHelper.isViewpointActive(capellaProject);
-        }
+  public boolean isActivityOk(ExplorerActivity activity) {
+    ActivityExplorerEditorInput editorInput = activity.getSection().getActivityExplorerPage().getEditorInput();
+    if (editorInput != null) {
+      Object adapter = editorInput.getAdapter(Session.class);
+      if (adapter instanceof Session) {
+        Session session = (Session) adapter;
+        Project capellaProject = SessionHelper.getCapellaProject(session);
+        return CyberSecurityViewpointHelper.isViewpointActive(capellaProject);
       }
-
     }
     return false;
   }
-
 }
