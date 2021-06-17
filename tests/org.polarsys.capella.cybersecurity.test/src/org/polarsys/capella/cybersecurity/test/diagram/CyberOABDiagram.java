c7a6c9a99114919b194bdc54e517f1da69e04b15
MalinaStefaniaStoicanescu <malina.stoicanescu@thalesgroup.com>
2021-06-17 09:57:44 +0300

    Add test for OAB
    Change-Id: I6d00209e18b05ea1cfd4bf9a6f8c1f70b0595ea9
    Signed-off-by: MalinaStefaniaStoicanescu <malina.stoicanescu@thalesgroup.com>

---
+++ tests/org.polarsys.capella.cybersecurity.test/src/org/polarsys/capella/cybersecurity/test/diagram/CyberOABDiagram.java
@@ -0,0 +1,67 @@
+/*******************************************************************************
+ * Copyright (c) 2021 THALES GLOBAL SERVICES.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *   
+ * Contributors:
+ *    Thales - initial API and implementation
+ *******************************************************************************/
+package org.polarsys.capella.cybersecurity.test.diagram;
+
+import org.eclipse.sirius.diagram.DDiagram;
+import org.polarsys.capella.core.sirius.analysis.IDiagramNameConstants;
+import org.polarsys.capella.core.sirius.analysis.constants.IToolNameConstants;
+import org.polarsys.capella.cybersecurity.model.PrimaryAsset;
+import org.polarsys.capella.cybersecurity.model.Threat;
+import org.polarsys.capella.cybersecurity.sirius.analysis.CybersecurityAnalysisConstants;
+import org.polarsys.capella.test.diagram.common.ju.context.DiagramContext;
+import org.polarsys.capella.test.diagram.common.ju.context.OABDiagram;
+import org.polarsys.capella.test.diagram.common.ju.step.crud.CreateDiagramStep;
+import org.polarsys.capella.test.diagram.common.ju.step.tools.CreatePathTool;
+import org.polarsys.capella.test.diagram.common.ju.step.tools.InsertRemoveTool;
+import org.polarsys.capella.test.framework.context.SessionContext;
+
+public class CyberOABDiagram extends OABDiagram {
+
+  public CyberOABDiagram(SessionContext context, DDiagram diagram) {
+    super(context, diagram);
+  }
+
+  public static CyberOABDiagram createDiagram(SessionContext context, String targetIdentifier) {
+    CyberOABDiagram diagram = (CyberOABDiagram) new CreateDiagramStep(context, targetIdentifier,
+        IDiagramNameConstants.OPERATIONAL_ENTITY_BLANK_DIAGRAM_NAME) {
+      @Override
+      public DiagramContext getResult() {
+        return new CyberOABDiagram(getExecutionContext(), diagram);
+      }
+    }.run().open();
+    CyberDiagramLayers.activateLayer(context.getSession(), diagram.getDiagram(),
+        CybersecurityAnalysisConstants.LAYER_ASSETS);
+    return diagram;
+
+  }
+
+  public void insertPrimaryAsset(PrimaryAsset pa) {
+    InsertRemoveTool irt = new InsertRemoveTool(this, CybersecurityAnalysisConstants.TOOL_XAB_INSERT_PA);
+    irt.insert(pa.getId());
+    irt.getResult();
+  }
+
+  public void removePrimaryAsset(PrimaryAsset pa) {
+    new InsertRemoveTool(this, CybersecurityAnalysisConstants.TOOL_XAB_INSERT_PA).remove(pa.getId());
+  }
+
+  public void insertThreat(Threat threat) {
+    new InsertRemoveTool(this, CybersecurityAnalysisConstants.TOOL_XAB_INSERT_THREAT).insert(threat.getId());
+  }
+  
+  @Override
+  public void createFunctionalChain(String path, String... links) {
+    new CreatePathTool(this, IToolNameConstants.TOOL_OAIB_CREATE_OPERATIONAL_PROCESS, path, links).run();
+  }
+
+}
