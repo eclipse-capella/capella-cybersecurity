68d42393ee2ddbe118ab2613e48b485a6e32881f
Georgiana-Elena ECOBICI <georgiana-elena.ecobici@thalesgroup.com>
2021-06-18 11:54:48 +0300

    Add realized fields in Property View of Cybersecurity elements
    Change-Id: I01b62d9fe47ec17418c3eaafe441fbbda0dc209f
    Signed-off-by: Georgiana-Elena ECOBICI
    <georgiana-elena.ecobici@thalesgroup.com>
    
    Add all elements
    
    Change-Id: I72cb5ac36151073c540ebc241c32d00259b2c2ef
    Signed-off-by: MalinaStefaniaStoicanescu
    <malina.stoicanescu@thalesgroup.com>
    
    
    Change-Id: I01b62d9fe47ec17418c3eaafe441fbbda0dc209f
    Signed-off-by: Georgiana-Elena ECOBICI
    <georgiana-elena.ecobici@thalesgroup.com>
    
    IPA)
    
    Change-Id: I9d524f28e9bd5faf592414c1c48c0bd096086c61
    Signed-off-by: MalinaStefaniaStoicanescu <malina.stoicanescu@thalesgroup.com>

---
+++ plugins/org.polarsys.capella.cybersecurity.ui/src/org/polarsys/capella/cybersecurity/ui/properties/sections/CybersecurityRealizationsController.java
@@ -0,0 +1,93 @@
+/*******************************************************************************
+ * Copyright (c) 2021 THALES GLOBAL SERVICES.
+ * 
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ * 
+ * Contributors:
+ *    Thales - initial API and implementation
+ *******************************************************************************/
+package org.polarsys.capella.cybersecurity.ui.properties.sections;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.polarsys.capella.common.data.modellingcore.ModellingcorePackage;
+import org.polarsys.capella.common.data.modellingcore.TraceableElement;
+import org.polarsys.capella.core.business.queries.IBusinessQuery;
+import org.polarsys.capella.core.business.queries.capellacore.BusinessQueriesProvider;
+import org.polarsys.capella.core.data.capellacommon.CapellacommonFactory;
+import org.polarsys.capella.core.data.capellacommon.TransfoLink;
+import org.polarsys.capella.core.data.cs.BlockArchitecture;
+import org.polarsys.capella.core.model.helpers.BlockArchitectureExt;
+import org.polarsys.capella.core.ui.properties.controllers.AbstractMultipleSemanticFieldController;
+import org.polarsys.capella.core.ui.properties.fields.AbstractSemanticField;
+
+/**
+ */
+public class CybersecurityRealizationsController extends AbstractMultipleSemanticFieldController {
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  protected IBusinessQuery getReadOpenValuesQuery(EObject semanticElement) {
+    return BusinessQueriesProvider.getInstance().getContribution(semanticElement.eClass(), ModellingcorePackage.Literals.TRACEABLE_ELEMENT__OUTGOING_TRACES);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public List<EObject> loadValues(EObject semanticElement, EStructuralFeature semanticFeature) {
+    List<EObject> values = new ArrayList<EObject>();
+    
+    Object lst = semanticElement.eGet(semanticFeature);
+    if (lst instanceof Collection<?>) {
+      for (Object obj : (Collection<?>) lst) {
+        if (obj instanceof TransfoLink) {
+          values.add(((TransfoLink) obj).getTarget());
+        }
+      }
+    }
+
+    return values;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  protected void doAddOperationInWriteOpenValues(EObject semanticElement, EStructuralFeature semanticFeature, EObject object) {
+    TransfoLink link = CapellacommonFactory.eINSTANCE.createTransfoLink();
+    link.setSourceElement((TraceableElement) semanticElement);
+    link.setTargetElement((TraceableElement) object);
+    BlockArchitecture ba = BlockArchitectureExt.getRootBlockArchitecture(semanticElement);
+    ba.getOwnedTraces().add(link);
+  }
+
+  /**
+   * Do the remove operation in {@link #writeOpenValues(EObject, EStructuralFeature, List)}
+   * @param semanticElement
+   * @param semanticFeature
+   * @param object
+   */
+  @SuppressWarnings("unchecked")
+  @Override
+  protected void doRemoveOperationInWriteOpenValues(EObject semanticElement, EStructuralFeature semanticFeature, EObject object) {
+    EObject linkToRemove = null;
+    for (EObject obj : (List<EObject>) semanticElement.eGet(semanticFeature)) {
+      if ((obj instanceof TransfoLink) && ((TransfoLink) obj).getTarget().equals(object)) {
+        linkToRemove = obj;
+      }
+    }
+    if (linkToRemove != null) {
+      AbstractSemanticField.deleteContainmentValue(linkToRemove);
+    }
+  }
+}
