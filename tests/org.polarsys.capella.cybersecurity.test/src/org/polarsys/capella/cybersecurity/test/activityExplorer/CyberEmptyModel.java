6329924f9e06a801dcf5df0df272c44ed43cc098
MalinaStefaniaStoicanescu <malina.stoicanescu@thalesgroup.com>
2021-07-02 10:15:05 +0300

    OA: Create TDB from Activity Explorer + JUnit
    Change-Id: I344e2e5f8bf46304f73850c96c393e7b7de6dfda
    Signed-off-by: MalinaStefaniaStoicanescu <malina.stoicanescu@thalesgroup.com>

--- tests/org.polarsys.capella.cybersecurity.test/src/org/polarsys/capella/cybersecurity/test/activityExplorer/CyberEmptyModel.java
+++ tests/org.polarsys.capella.cybersecurity.test/src/org/polarsys/capella/cybersecurity/test/activityExplorer/CyberEmptyModel.java
@@ -18,6 +18,8 @@ import java.util.List;
 import org.polarsys.capella.test.framework.api.NonDirtyTestCase;
 
 public abstract class CyberEmptyModel extends NonDirtyTestCase {
+  public final static String OA = "d4beedad-e43c-4073-845d-b8d6c8f28542";
+  public final static String OA_ENTITIES = "f6985216-39d9-48a0-8b3d-ab47621b6b82";
   public final static String LA__LOGICAL_COMPONENT = "709a8dd2-a707-4bd7-8871-48e52405d24d";
   public final static String PA__PHYSICAL_COMPONENT = "b3b4a2c0-6e76-4474-9082-6c6bb7d058d6";
   public final static String SA__SYSTEM_COMPONENT = "aad3c686-3612-44c0-9ce8-4bb74e1093d0";
