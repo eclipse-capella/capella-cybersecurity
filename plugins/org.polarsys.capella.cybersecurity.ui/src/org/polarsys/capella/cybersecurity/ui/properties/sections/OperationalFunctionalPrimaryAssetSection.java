2e3b0daff31cac1870d330ef3b966c4c27333f94
Georgiana-Elena ECOBICI <georgiana-elena.ecobici@thalesgroup.com>
2021-07-14 14:55:54 +0300

    #11 OA level shall not show Realized selection widget
    Change-Id: I9ec9bf03ec1df56197dfc0ab12e2cafe49044a98
    Signed-off-by: Georgiana-Elena ECOBICI <georgiana-elena.ecobici@thalesgroup.com>

---
+++ plugins/org.polarsys.capella.cybersecurity.ui/src/org/polarsys/capella/cybersecurity/ui/properties/sections/OperationalFunctionalPrimaryAssetSection.java
@@ -0,0 +1,28 @@
+/*******************************************************************************
+ * Copyright (c) 2019 THALES GLOBAL SERVICES.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *   
+ * Contributors:
+ *    Thales - initial API and implementation
+ *******************************************************************************/
+package org.polarsys.capella.cybersecurity.ui.properties.sections;
+
+import org.eclipse.emf.ecore.EObject;
+import org.polarsys.capella.cybersecurity.model.FunctionalPrimaryAsset;
+
+public class OperationalFunctionalPrimaryAssetSection extends FunctionalPrimaryAssetSection {
+  public OperationalFunctionalPrimaryAssetSection() {
+    super(false);
+  }
+  
+  @Override
+  public boolean select(Object toTest) {
+    EObject eObjectToTest = super.selection(toTest);
+    return eObjectToTest instanceof FunctionalPrimaryAsset && !isRealizedArchitecture(eObjectToTest);
+  }
+}
---
+++ tests/org.polarsys.capella.cybersecurity.test/src/org/polarsys/capella/cybersecurity/test/queries/FunctionPrimaryAssetsRealizedTest.java
@@ -0,0 +1,53 @@
+/*******************************************************************************
+ * Copyright (c) 2019 THALES GLOBAL SERVICES.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *   
+ * Contributors:
+ *    Thales - initial API and implementation
+ *******************************************************************************/
+package org.polarsys.capella.cybersecurity.test.queries;
+
+import java.util.List;
+
+import org.eclipse.emf.ecore.EObject;
+import org.polarsys.capella.common.ef.command.AbstractReadWriteCommand;
+import org.polarsys.capella.common.helpers.TransactionHelper;
+import org.polarsys.capella.cybersecurity.business.queries.FunctionalPrimaryAsset_RealizedPrimaryAssets;
+import org.polarsys.capella.cybersecurity.model.CybersecurityFactory;
+import org.polarsys.capella.cybersecurity.model.CybersecurityPkg;
+import org.polarsys.capella.cybersecurity.model.PrimaryAsset;
+import org.polarsys.capella.cybersecurity.sirius.analysis.CybersecurityServices;
+
+public class FunctionPrimaryAssetsRealizedTest extends CybersecurityQueriesTest {
+  PrimaryAsset oaPA;
+  PrimaryAsset saPA;
+
+  protected void init() {
+    super.init();
+    oaPA = CybersecurityFactory.eINSTANCE.createFunctionalPrimaryAsset();
+    saPA = CybersecurityFactory.eINSTANCE.createFunctionalPrimaryAsset();
+    
+    AbstractReadWriteCommand cmd = new AbstractReadWriteCommand() {
+      @Override
+      public void run() {
+        CybersecurityPkg oaPkg = new CybersecurityServices().getDefaultCyberSecurityPackage(oaArch, true);
+        oaPkg.getOwnedPrimaryAssets().add(oaPA);
+        CybersecurityPkg saPkg = new CybersecurityServices().getDefaultCyberSecurityPackage(saArch, true);
+        saPkg.getOwnedPrimaryAssets().add(saPA);
+      }
+    };
+    TransactionHelper.getExecutionManager(project).execute(cmd);
+  }
+  
+  @Override
+  protected void doTest() {
+    List<EObject> elements = new FunctionalPrimaryAsset_RealizedPrimaryAssets().getAvailableElements(saPA);
+    assertTrue(elements.contains(oaPA));
+    assertFalse(elements.contains(saPA));
+  }
+}
