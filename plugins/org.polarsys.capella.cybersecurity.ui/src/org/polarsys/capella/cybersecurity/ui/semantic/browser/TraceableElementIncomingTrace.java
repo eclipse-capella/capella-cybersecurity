6f5ace31622066cf565e7521f901bb6fed73bed7
Anita Matei <matei.anita@thalesgroup.com>
2021-07-20 16:23:50 +0300

    #16 Added new license, bump to 1.0.0
    Change-Id: I031ccb676f914ff47617d0a3244f1d49ff10adef
    Signed-off-by: Anita Matei <matei.anita@thalesgroup.com>

--- plugins/org.polarsys.capella.cybersecurity.ui/src/org/polarsys/capella/cybersecurity/ui/semantic/browser/TraceableElementIncomingTrace.java
+++ plugins/org.polarsys.capella.cybersecurity.ui/src/org/polarsys/capella/cybersecurity/ui/semantic/browser/TraceableElementIncomingTrace.java
@@ -1,28 +1,25 @@
-/*******************************************************************************
- * Copyright (c) 2021 THALES GLOBAL SERVICES.
- *
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License 2.0 which is available at
- * http://www.eclipse.org/legal/epl-2.0
- *
- * SPDX-License-Identifier: EPL-2.0
- *   
- * Contributors:
- *    Thales - initial API and implementation
- *******************************************************************************/
+/*******************************************************************
+* Copyright © 2021 Thales Global Services SAS
+*
+* Licensed under the Thales Inner Source Software License:
+* Version 1.2, InnerOpen - Distribution Controlled
+*
+* You may not use this file except in compliance with the License.
+* You may obtain a copy of the License at https://gitlab.thalesdigital.io/Tiss-Organization/tiss-licenses
+* See the License for the specific language governing permissions and limitations under the License.
+*******************************************************************/
 package org.polarsys.capella.cybersecurity.ui.semantic.browser;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.emf.common.util.EList;
-
-import org.polarsys.capella.core.data.capellacommon.GenericTrace;
-import org.polarsys.capella.cybersecurity.model.PrimaryAsset;
-import org.polarsys.capella.cybersecurity.model.Threat;
 import org.polarsys.capella.common.data.modellingcore.AbstractTrace;
 import org.polarsys.capella.common.data.modellingcore.TraceableElement;
 import org.polarsys.capella.common.helpers.query.IQuery;
+import org.polarsys.capella.core.data.capellacommon.GenericTrace;
+import org.polarsys.capella.cybersecurity.model.PrimaryAsset;
+import org.polarsys.capella.cybersecurity.model.Threat;
 
 public class TraceableElementIncomingTrace implements IQuery {
 
--- tests/org.polarsys.capella.cybersecurity.test/src/org/polarsys/capella/cybersecurity/test/activityExplorer/OperationalTDBActivityExplorerTestCase.java
+++ tests/org.polarsys.capella.cybersecurity.test/src/org/polarsys/capella/cybersecurity/test/activityExplorer/OperationalTDBActivityExplorerTestCase.java
@@ -1,13 +1,13 @@
-/*******************************************************************************
- * Copyright (c) 2021 THALES GLOBAL SERVICES.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *  
- * Contributors:
- *    Thales - initial API and implementation
- *******************************************************************************/
+/*******************************************************************
+* Copyright © 2021 Thales Global Services SAS
+*
+* Licensed under the Thales Inner Source Software License:
+* Version 1.2, InnerOpen - Distribution Controlled
+*
+* You may not use this file except in compliance with the License.
+* You may obtain a copy of the License at https://gitlab.thalesdigital.io/Tiss-Organization/tiss-licenses
+* See the License for the specific language governing permissions and limitations under the License.
+*******************************************************************/
 package org.polarsys.capella.cybersecurity.test.activityExplorer;
 
 import java.util.Collection;
