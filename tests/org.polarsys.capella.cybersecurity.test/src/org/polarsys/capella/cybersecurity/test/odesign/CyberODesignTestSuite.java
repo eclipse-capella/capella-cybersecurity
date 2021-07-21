0d08919e29ab5dfee2e0a6eb8b29c24f4e06899d
Anita Matei <matei.anita@thalesgroup.com>
2021-07-21 14:52:19 +0300

    Added delete descriptions and direct edit labels for all cyber elements.
    Change-Id: I07cfb9be28f9afc6f0aa630cda503c631d9d1da0
    Signed-off-by: Anita Matei <matei.anita@thalesgroup.com>

---
+++ tests/org.polarsys.capella.cybersecurity.test/src/org/polarsys/capella/cybersecurity/test/odesign/CyberODesignTestSuite.java
@@ -0,0 +1,37 @@
+/*******************************************************************************
+ * Copyright (c) 2021 THALES GLOBAL SERVICES.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *   
+ * Contributors:
+ *    Thales - initial API and implementation
+ *******************************************************************************/
+package org.polarsys.capella.cybersecurity.test.odesign;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.polarsys.capella.test.framework.api.BasicTestArtefact;
+import org.polarsys.capella.test.framework.api.BasicTestSuite;
+
+import junit.framework.Test;
+
+public class CyberODesignTestSuite extends BasicTestSuite {
+
+  public static Test suite() {
+    return new CyberODesignTestSuite();
+  }
+
+  @Override
+  protected List<BasicTestArtefact> getTests() {
+    List<BasicTestArtefact> tests = new ArrayList<BasicTestArtefact>();
+    tests.add(new CyberElementsHaveDeleteDescriptionTest());
+    tests.add(new CyberElementsHaveDirectEditLabelTest());
+    return tests;
+  }
+
+}
