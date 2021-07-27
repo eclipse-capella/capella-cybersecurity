3929c095a1a949e0e98b6ecdc986c193f15f9f02
Georgiana-Elena ECOBICI <georgiana-elena.ecobici@thalesgroup.com>
2021-07-27 23:15:12 +0300

    #107 Fix icon and label on OA on create Functional Primary Assets
    Change-Id: If67e33b738d6fcb09e16df9865476ff9c0fc7710
    Signed-off-by: Georgiana-Elena ECOBICI <georgiana-elena.ecobici@thalesgroup.com>

--- plugins/org.polarsys.capella.cybersecurity.model.edit.decorators/src/org/polarsys/capella/cybersecurity/model/edit/decorators/FunctionalPrimaryAssetProviderDecorator.java
+++ plugins/org.polarsys.capella.cybersecurity.model.edit.decorators/src/org/polarsys/capella/cybersecurity/model/edit/decorators/FunctionalPrimaryAssetProviderDecorator.java
@@ -12,27 +12,24 @@
  *******************************************************************************/
 package org.polarsys.capella.cybersecurity.model.edit.decorators;
 
-import java.util.Arrays;
-
 import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.edit.provider.ComposedImage;
 import org.eclipse.emf.edit.provider.IEditingDomainItemProvider;
 import org.eclipse.emf.edit.provider.IItemLabelProvider;
 import org.eclipse.emf.edit.provider.IItemPropertySource;
 import org.eclipse.emf.edit.provider.IStructuredItemContentProvider;
 import org.eclipse.emf.edit.provider.ITreeItemContentProvider;
-import org.polarsys.capella.core.data.capellamodeller.provider.CapellaModellerEditPlugin;
 import org.polarsys.capella.core.data.cs.BlockArchitecture;
-import org.polarsys.capella.core.data.ctx.SystemAnalysis;
 import org.polarsys.capella.core.data.gen.edit.decorators.ItemProviderAdapterDecorator;
-import org.polarsys.capella.core.data.la.LogicalArchitecture;
 import org.polarsys.capella.core.data.oa.OperationalAnalysis;
 import org.polarsys.capella.core.model.helpers.BlockArchitectureExt;
 import org.polarsys.capella.cybersecurity.model.provider.CybersecurityEditPlugin;
 
 public class FunctionalPrimaryAssetProviderDecorator extends ItemProviderAdapterDecorator implements IEditingDomainItemProvider,
     IStructuredItemContentProvider, ITreeItemContentProvider, IItemLabelProvider, IItemPropertySource {
+  
+  private static final String OA_PRIMARY_ASSETS_PREFIX = "OperationalActivity";
+  private static final String FUNCTIONAL_PRIMARY_ASSETS_PREFIX = "Functional";
 
   public FunctionalPrimaryAssetProviderDecorator(AdapterFactory adapterFactory) {
     super(adapterFactory);
@@ -41,15 +38,21 @@ public class FunctionalPrimaryAssetProviderDecorator extends ItemProviderAdapter
   @Override
   public Object getImage(Object object) {
     BlockArchitecture architecture = BlockArchitectureExt.getRootBlockArchitecture((EObject)object);
-    String image = "full/obj16/PhysicalFunction";
+    String image = "full/obj16/FunctionalPrimaryAssetDecorated";
     if (architecture instanceof OperationalAnalysis) {
-      image = "full/obj16/OperationalActivity";
-    } else if (architecture instanceof SystemAnalysis) {
-      image = "full/obj16/SystemFunction";
-    } else if (architecture instanceof LogicalArchitecture) {
-      image = "full/obj16/LogicalFunction";
+      image = "full/obj16/OperationalActivityPrimaryAssetDecorated";
     }
-    return new ComposedImage(Arrays.asList(CapellaModellerEditPlugin.INSTANCE.getImage(image),
-        CybersecurityEditPlugin.INSTANCE.getImage("full/ovr16/PrimaryAssetDecorator")));
+    
+    return overlayImage(object, CybersecurityEditPlugin.INSTANCE.getImage(image));
+  }
+  
+  @Override
+  public String getText(Object object) {
+    BlockArchitecture architecture = BlockArchitectureExt.getRootBlockArchitecture((EObject)object);
+    String text = super.getText(object);
+    if (architecture instanceof OperationalAnalysis) {
+      text = text.replace(FUNCTIONAL_PRIMARY_ASSETS_PREFIX, OA_PRIMARY_ASSETS_PREFIX);
+    }
+    return text;
   }
 }
