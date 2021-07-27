118832803cbd08319109d461b918cbf8cb28ac8f
Anita Matei <matei.anita@thalesgroup.com>
2021-07-27 17:06:06 +0300

    #18 Replaced the label for Functional Primary Assets on the OA level for validation rules
    Change-Id: I88c3212c78e6814cf25edd047bd48e8f6d3258bc
    Signed-off-by: Anita Matei <matei.anita@thalesgroup.com>

--- plugins/org.polarsys.capella.cybersecurity.validation/src/org/polarsys/capella/cybersecurity/validation/design/coverage/CY_DCOV_04_PrimaryAssetWithoutThread.java
+++ plugins/org.polarsys.capella.cybersecurity.validation/src/org/polarsys/capella/cybersecurity/validation/design/coverage/CY_DCOV_04_PrimaryAssetWithoutThread.java
@@ -15,9 +15,9 @@ package org.polarsys.capella.cybersecurity.validation.design.coverage;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.emf.validation.AbstractModelConstraint;
 import org.eclipse.emf.validation.IValidationContext;
-import org.polarsys.capella.core.model.utils.NamingHelper;
 import org.polarsys.capella.cybersecurity.model.CybersecurityQueries;
 import org.polarsys.capella.cybersecurity.model.PrimaryAsset;
+import org.polarsys.capella.cybersecurity.validation.util.CyberValidationHelper;
 
 /**
  * A PrimaryAsset must be threatened by a Threat
@@ -28,7 +28,7 @@ public class CY_DCOV_04_PrimaryAssetWithoutThread extends AbstractModelConstrain
   public IStatus validate(IValidationContext ctx) {
     PrimaryAsset pa = (PrimaryAsset) ctx.getTarget();
     if (!CybersecurityQueries.getThreatsOf(pa).findAny().isPresent()) {
-      return ctx.createFailureStatus(pa, NamingHelper.getTitleLabel(pa));
+      return ctx.createFailureStatus(pa, CyberValidationHelper.getPATitleLabel(pa));
     }
     return ctx.createSuccessStatus();
   }
