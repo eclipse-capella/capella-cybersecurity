0fdd937266e14a71117150f828e4828aa0e9e6e7
Anita Matei <matei.anita@thalesgroup.com>
2021-07-29 13:41:41 +0300

    #109 Fixed transition bug displaying changes when there are none
    Change-Id: I843d86d10913b9fa179db68a3881b9274c857132
    Signed-off-by: Anita Matei <matei.anita@thalesgroup.com>

--- plugins/org.polarsys.capella.cybersecurity.transition/src/org/polarsys/capella/cybersecurity/transition/rules/SecurityNeedsRule.java
+++ plugins/org.polarsys.capella.cybersecurity.transition/src/org/polarsys/capella/cybersecurity/transition/rules/SecurityNeedsRule.java
@@ -10,12 +10,17 @@
 *******************************************************************/
 package org.polarsys.capella.cybersecurity.transition.rules;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
+import org.polarsys.capella.core.transition.common.handlers.attachment.AttachmentHelper;
 import org.polarsys.capella.core.transition.common.rules.AbstractUpdateRule;
 import org.polarsys.capella.cybersecurity.model.CybersecurityPackage;
 import org.polarsys.capella.cybersecurity.model.SecurityNeeds;
 import org.polarsys.kitalpha.transposer.rules.handler.rules.api.IContext;
+import org.polarsys.kitalpha.transposer.rules.handler.rules.api.IPremise;
 
 public class SecurityNeedsRule extends AbstractUpdateRule {
 
@@ -37,13 +42,47 @@ public class SecurityNeedsRule extends AbstractUpdateRule {
   }
 
   @Override
-  protected void updateElement(EObject element, EObject result, IContext context) {
-    super.updateElement(element, result, context);
-    SecurityNeeds paResult = (SecurityNeeds) result;
-    SecurityNeeds paSource = (SecurityNeeds) element;
-    paResult.setAvailabilityValue(paSource.getAvailabilityValue());
-    paResult.setConfidentialityValue(paSource.getConfidentialityValue());
-    paResult.setIntegrityValue(paSource.getIntegrityValue());
-    paResult.setTraceabilityValue(paSource.getTraceabilityValue());
+  protected void premicesRelated(EObject element, ArrayList<IPremise> needed) {
+    super.premicesRelated(element, needed);
+    needed.addAll(
+        createDefaultPrecedencePremices(element, CybersecurityPackage.Literals.SECURITY_NEEDS__CONFIDENTIALITY_VALUE));
+    needed.addAll(
+        createDefaultPrecedencePremices(element, CybersecurityPackage.Literals.SECURITY_NEEDS__INTEGRITY_VALUE));
+    needed.addAll(
+        createDefaultPrecedencePremices(element, CybersecurityPackage.Literals.SECURITY_NEEDS__AVAILABILITY_VALUE));
+    needed.addAll(
+        createDefaultPrecedencePremices(element, CybersecurityPackage.Literals.SECURITY_NEEDS__TRACEABILITY_VALUE));
   }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  protected void attachRelated(EObject element, EObject result, IContext context) {
+    super.attachRelated(element, result, context);
+    AttachmentHelper.getInstance(context).attachTracedElements(element, result,
+        CybersecurityPackage.Literals.SECURITY_NEEDS__CONFIDENTIALITY_VALUE, context);
+    AttachmentHelper.getInstance(context).attachTracedElements(element, result,
+        CybersecurityPackage.Literals.SECURITY_NEEDS__INTEGRITY_VALUE, context);
+    AttachmentHelper.getInstance(context).attachTracedElements(element, result,
+        CybersecurityPackage.Literals.SECURITY_NEEDS__AVAILABILITY_VALUE, context);
+    AttachmentHelper.getInstance(context).attachTracedElements(element, result,
+        CybersecurityPackage.Literals.SECURITY_NEEDS__TRACEABILITY_VALUE, context);
+  }
+
+  @Override
+  protected void retrieveGoDeep(EObject source, List<EObject> result, IContext context) {
+    super.retrieveGoDeep(source, result, context);
+
+    SecurityNeeds element = (SecurityNeeds) source;
+    if (element.getConfidentialityValue() != null)
+      result.add(element.getConfidentialityValue().eContainer().eContainer());
+    else if (element.getAvailabilityValue() != null)
+      result.add(element.getAvailabilityValue().eContainer().eContainer());
+    else if (element.getIntegrityValue() != null)
+      result.add(element.getIntegrityValue().eContainer().eContainer());
+    else if (element.getTraceabilityValue() != null)
+      result.add(element.getTraceabilityValue().eContainer().eContainer());
+  }
+
 }
