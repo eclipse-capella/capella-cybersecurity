4d90729b711c858bb7c167d869925cda545e7c42
Georgiana-Elena ECOBICI <georgiana-elena.ecobici@thalesgroup.com>
2021-08-04 14:59:46 +0300

    #26 Add Realized/Realizing information in Threats and PrimaryAssets
    Change-Id: Iada600228762d3c25804ff5fe442a67b74e81763
    Signed-off-by: Georgiana-Elena ECOBICI <georgiana-elena.ecobici@thalesgroup.com>

--- plugins/org.polarsys.capella.cybersecurity.model/src/org/polarsys/capella/cybersecurity/model/impl/FunctionalPrimaryAssetImpl.java
+++ plugins/org.polarsys.capella.cybersecurity.model/src/org/polarsys/capella/cybersecurity/model/impl/FunctionalPrimaryAssetImpl.java
@@ -16,10 +16,16 @@ package org.polarsys.capella.cybersecurity.model.impl;
 import java.util.ArrayList;
 import java.util.Collection;
 
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.IAdapterManager;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EAnnotation;
 import org.eclipse.emf.ecore.EClass;
-import org.eclipse.emf.ecore.util.EObjectResolvingEList;
+import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.util.EcoreEList;
+import org.polarsys.capella.common.model.helpers.IHelper;
+import org.polarsys.capella.core.data.cs.Component;
 import org.polarsys.capella.core.data.fa.AbstractFunction;
 import org.polarsys.capella.core.data.fa.FunctionalChain;
 import org.polarsys.capella.cybersecurity.model.CybersecurityPackage;
@@ -35,6 +41,8 @@ import org.polarsys.capella.cybersecurity.model.PrimaryAssetMember;
  * <ul>
  *   <li>{@link org.polarsys.capella.cybersecurity.model.impl.FunctionalPrimaryAssetImpl#getFunctions <em>Functions</em>}</li>
  *   <li>{@link org.polarsys.capella.cybersecurity.model.impl.FunctionalPrimaryAssetImpl#getFunctionalChains <em>Functional Chains</em>}</li>
+ *   <li>{@link org.polarsys.capella.cybersecurity.model.impl.FunctionalPrimaryAssetImpl#getRealizedPrimaryAssets <em>Realized Primary Assets</em>}</li>
+ *   <li>{@link org.polarsys.capella.cybersecurity.model.impl.FunctionalPrimaryAssetImpl#getRealizingPrimaryAssets <em>Realizing Primary Assets</em>}</li>
  * </ul>
  *
  * @generated
@@ -97,6 +105,95 @@ public class FunctionalPrimaryAssetImpl extends PrimaryAssetImpl implements Func
   }
 
   /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated NOT
+   */
+
+  @Override
+  public EList<FunctionalPrimaryAsset> getRealizedPrimaryAssets() {
+    Object result = null;
+    // Helper that can get value for current feature.
+    IHelper helper = null;
+    // If current object is adaptable, ask it to get its IHelper.
+    if (this instanceof IAdaptable) {
+      helper = (IHelper) ((IAdaptable) this).getAdapter(IHelper.class);
+    }
+    if (null == helper) {
+      // No helper found yet.
+      // Ask the platform to get the adapter 'IHelper.class' for current object.
+      IAdapterManager adapterManager = Platform.getAdapterManager();
+      helper = (IHelper) adapterManager.getAdapter(this, IHelper.class);
+    }
+    if (null == helper) {
+      EPackage package_l = eClass().getEPackage();
+      // Get the root package of the owner package.
+      EPackage rootPackage = org.polarsys.capella.common.mdsofa.common.helper.EcoreHelper.getRootPackage(package_l);
+      throw new org.polarsys.capella.common.model.helpers.HelperNotFoundException(
+          "No helper retrieved for nsURI " + rootPackage.getNsURI()); //$NON-NLS-1$
+    }
+    // A helper is found, let's use it. 
+    EAnnotation annotation = CybersecurityPackage.Literals.FUNCTIONAL_PRIMARY_ASSET__REALIZED_PRIMARY_ASSETS
+        .getEAnnotation(org.polarsys.capella.common.model.helpers.IModelConstants.HELPER_ANNOTATION_SOURCE);
+    result = helper.getValue(this, CybersecurityPackage.Literals.FUNCTIONAL_PRIMARY_ASSET__REALIZED_PRIMARY_ASSETS, annotation);
+
+    try {
+      @SuppressWarnings("unchecked")
+      Collection<Component> resultAsList = (Collection<Component>) result;
+      return new EcoreEList.UnmodifiableEList<FunctionalPrimaryAsset>(this, CybersecurityPackage.Literals.FUNCTIONAL_PRIMARY_ASSET__REALIZED_PRIMARY_ASSETS,
+          resultAsList.size(), resultAsList.toArray());
+    } catch (ClassCastException exception) {
+      exception.printStackTrace();
+      return org.eclipse.emf.common.util.ECollections.emptyEList();
+    }
+
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated NOT
+   */
+
+  @Override
+  public EList<FunctionalPrimaryAsset> getRealizingPrimaryAssets() {
+    Object result = null;
+    // Helper that can get value for current feature.
+    IHelper helper = null;
+    // If current object is adaptable, ask it to get its IHelper.
+    if (this instanceof IAdaptable) {
+      helper = (IHelper) ((IAdaptable) this).getAdapter(IHelper.class);
+    }
+    if (null == helper) {
+      // No helper found yet.
+      // Ask the platform to get the adapter 'IHelper.class' for current object.
+      IAdapterManager adapterManager = Platform.getAdapterManager();
+      helper = (IHelper) adapterManager.getAdapter(this, IHelper.class);
+    }
+    if (null == helper) {
+      EPackage package_l = eClass().getEPackage();
+      // Get the root package of the owner package.
+      EPackage rootPackage = org.polarsys.capella.common.mdsofa.common.helper.EcoreHelper.getRootPackage(package_l);
+      throw new org.polarsys.capella.common.model.helpers.HelperNotFoundException(
+          "No helper retrieved for nsURI " + rootPackage.getNsURI()); //$NON-NLS-1$
+    }
+    // A helper is found, let's use it. 
+    EAnnotation annotation = CybersecurityPackage.Literals.FUNCTIONAL_PRIMARY_ASSET__REALIZING_PRIMARY_ASSETS
+        .getEAnnotation(org.polarsys.capella.common.model.helpers.IModelConstants.HELPER_ANNOTATION_SOURCE);
+    result = helper.getValue(this, CybersecurityPackage.Literals.FUNCTIONAL_PRIMARY_ASSET__REALIZING_PRIMARY_ASSETS, annotation);
+
+    try {
+      @SuppressWarnings("unchecked")
+      Collection<Component> resultAsList = (Collection<Component>) result;
+      return new EcoreEList.UnmodifiableEList<FunctionalPrimaryAsset>(this, CybersecurityPackage.Literals.FUNCTIONAL_PRIMARY_ASSET__REALIZING_PRIMARY_ASSETS,
+          resultAsList.size(), resultAsList.toArray());
+    } catch (ClassCastException exception) {
+      exception.printStackTrace();
+      return org.eclipse.emf.common.util.ECollections.emptyEList();
+    }
+  }
+
+  /**
    * <!-- begin-user-doc --> <!-- end-user-doc -->
    * @generated
    */
@@ -107,6 +204,10 @@ public class FunctionalPrimaryAssetImpl extends PrimaryAssetImpl implements Func
       return getFunctions();
     case CybersecurityPackage.FUNCTIONAL_PRIMARY_ASSET__FUNCTIONAL_CHAINS:
       return getFunctionalChains();
+    case CybersecurityPackage.FUNCTIONAL_PRIMARY_ASSET__REALIZED_PRIMARY_ASSETS:
+      return getRealizedPrimaryAssets();
+    case CybersecurityPackage.FUNCTIONAL_PRIMARY_ASSET__REALIZING_PRIMARY_ASSETS:
+      return getRealizingPrimaryAssets();
     }
     return super.eGet(featureID, resolve, coreType);
   }
@@ -122,6 +223,10 @@ public class FunctionalPrimaryAssetImpl extends PrimaryAssetImpl implements Func
       return !getFunctions().isEmpty();
     case CybersecurityPackage.FUNCTIONAL_PRIMARY_ASSET__FUNCTIONAL_CHAINS:
       return !getFunctionalChains().isEmpty();
+    case CybersecurityPackage.FUNCTIONAL_PRIMARY_ASSET__REALIZED_PRIMARY_ASSETS:
+      return !getRealizedPrimaryAssets().isEmpty();
+    case CybersecurityPackage.FUNCTIONAL_PRIMARY_ASSET__REALIZING_PRIMARY_ASSETS:
+      return !getRealizingPrimaryAssets().isEmpty();
     }
     return super.eIsSet(featureID);
   }
