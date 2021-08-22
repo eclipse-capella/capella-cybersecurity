3c7ae75b209005231fa5319bd21bd64647cfb301
Georgiana-Elena ECOBICI <georgiana-elena.ecobici@thalesgroup.com>
2021-08-22 17:33:57 +0300

    #123 Automated transitions of Cybersecurity Elements in Activity Explorer
    Change-Id: I64b81c849422d3d98a5cf124670707209df0514b
    Signed-off-by: Georgiana-Elena ECOBICI <georgiana-elena.ecobici@thalesgroup.com>

---
+++ tests/org.polarsys.capella.cybersecurity.test/src/org/polarsys/capella/cybersecurity/test/activityExplorer/CyberTransitionsFromLAActivityExplorerTestCase.java
@@ -0,0 +1,47 @@
+/*******************************************************************
+* Copyright © 2021 Thales Global Services SAS
+*
+* Licensed under the Thales Inner Source Software License:
+* Version 1.2, InnerOpen - Distribution Controlled
+*
+* You may not use this file except in compliance with the License.
+* You may obtain a copy of the License at https://gitlab.thalesdigital.io/Tiss-Organization/tiss-licenses
+* See the License for the specific language governing permissions and limitations under the License.
+*******************************************************************/
+package org.polarsys.capella.cybersecurity.test.activityExplorer;
+
+import java.util.Collections;
+
+import org.eclipse.amalgam.explorer.activity.ui.api.hyperlinkadapter.AbstractHyperlinkAdapter;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.sirius.business.api.session.Session;
+import org.eclipse.ui.forms.events.HyperlinkEvent;
+import org.polarsys.capella.common.data.modellingcore.ModelElement;
+import org.polarsys.capella.common.helpers.TransactionHelper;
+import org.polarsys.capella.cybersecurity.test.transition.CyberTransitionCommandHelper;
+import org.polarsys.capella.cybersecurity.ui.activity.PAPerformAutomatedTransitionAdapter;
+
+public class CyberTransitionsFromLAActivityExplorerTestCase extends CyberTransitionsActivityExplorerTestCase {
+  @Override
+  protected AbstractHyperlinkAdapter createLink() {
+    return new PAPerformAutomatedTransitionAdapter() {
+      @Override
+      public void linkPressed(HyperlinkEvent event, EObject rootSemanticModel, Session session) {
+        ModelElement modelElement = getModelElement(rootSemanticModel);
+        if (modelElement != null) {
+          TransactionHelper.getExecutionManager(modelElement)
+              .execute(CyberTransitionCommandHelper
+                  .getInstance().getCybersecurityTransitionCommand(Collections.singleton((Object) modelElement),
+                      new NullProgressMonitor()));
+
+        }
+      }
+    };
+  }
+
+  @Override
+  protected EObject getStructure() {
+    return context.getSemanticElement(LA__CYBERSECURITY_PKG);
+  }
+}
