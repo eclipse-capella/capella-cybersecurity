e6695183095e4a75bed8c44038823a432a8ca87b
Georgiana-Elena ECOBICI <georgiana-elena.ecobici@thalesgroup.com>
2021-08-22 17:33:57 +0300

    #123 Automated transitions of Cybersecurity Elements in Activity Explorer
    Change-Id: I64b81c849422d3d98a5cf124670707209df0514b
    Signed-off-by: Georgiana-Elena ECOBICI <georgiana-elena.ecobici@thalesgroup.com>

---
+++ tests/org.polarsys.capella.cybersecurity.test/src/org/polarsys/capella/cybersecurity/test/activityExplorer/ActivityExplorerTestCase.java
@@ -0,0 +1,46 @@
+package org.polarsys.capella.cybersecurity.test.activityExplorer;
+import org.eclipse.amalgam.explorer.activity.ui.api.hyperlinkadapter.AbstractHyperlinkAdapter;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.sirius.business.api.session.Session;
+import org.polarsys.capella.common.data.modellingcore.ModelElement;
+import org.polarsys.capella.core.data.capellamodeller.Project;
+import org.polarsys.capella.core.libraries.model.CapellaModel;
+import org.polarsys.capella.test.framework.context.SessionContext;
+
+public abstract class ActivityExplorerTestCase extends CyberEmptyModel {
+  Session session;
+  SessionContext context;
+  CapellaModel model;
+  Project project;
+  AbstractHyperlinkAdapter link;
+  EObject structure;
+  
+  protected abstract EObject getStructure();
+  
+  protected abstract AbstractHyperlinkAdapter createLink();
+
+  protected abstract ModelElement getTestModelElement();
+
+  protected void preTest() {
+    ModelElement modelElement = getTestModelElement();
+    assertEquals(modelElement, structure);
+  }
+
+  protected abstract void doTest();
+
+  protected void initialize() {
+    session = getSessionForTestModel(getRequiredTestModels().get(0));
+    context = new SessionContext(session);
+    model = getTestModel(getRequiredTestModels().get(0));
+    project = model.getProject(session.getTransactionalEditingDomain());
+    structure = getStructure();
+    link = createLink();
+  }
+
+  @Override
+  public void test() throws Exception {
+    initialize();
+    preTest();
+    doTest();
+  }
+}
--- tests/org.polarsys.capella.cybersecurity.test/src/org/polarsys/capella/cybersecurity/test/activityExplorer/CyberDiagramActivityExplorerTestCase.java
+++ tests/org.polarsys.capella.cybersecurity.test/src/org/polarsys/capella/cybersecurity/test/activityExplorer/CyberDiagramActivityExplorerTestCase.java
@@ -15,28 +15,13 @@ import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.eclipse.emf.ecore.EObject;
-import org.eclipse.sirius.business.api.session.Session;
 import org.eclipse.sirius.viewpoint.DRepresentationDescriptor;
 import org.polarsys.capella.common.data.modellingcore.ModelElement;
-import org.polarsys.capella.core.data.capellamodeller.Project;
 import org.polarsys.capella.core.explorer.activity.ui.hyperlinkadapter.AbstractCapellaNewDiagramHyperlinkAdapter;
-import org.polarsys.capella.core.libraries.model.CapellaModel;
 import org.polarsys.capella.core.model.handler.helpers.RepresentationHelper;
-import org.polarsys.capella.test.framework.context.SessionContext;
 import org.polarsys.capella.test.framework.helpers.GuiActions;
 
-public abstract class CyberDiagramActivityExplorerTestCase extends CyberEmptyModel {
-  Session session;
-  SessionContext context;
-  CapellaModel model;
-  Project project;
-  AbstractCapellaNewDiagramHyperlinkAdapter link;
-  EObject structure;
-
-  protected abstract AbstractCapellaNewDiagramHyperlinkAdapter createLink();
-
-  protected abstract EObject getStructure();
+public abstract class CyberDiagramActivityExplorerTestCase extends ActivityExplorerTestCase {
 
   protected abstract String getDefaultName();
 
@@ -52,23 +37,10 @@ public abstract class CyberDiagramActivityExplorerTestCase extends CyberEmptyMod
   }
 
   protected boolean getResultOfCreateDiagram() {
-    return link.createDiagram(structure, session);
-  }
+    if (link instanceof AbstractCapellaNewDiagramHyperlinkAdapter)
+      return ((AbstractCapellaNewDiagramHyperlinkAdapter) link).createDiagram(structure, session);
 
-  protected void initialize() {
-    session = getSessionForTestModel(getRequiredTestModels().get(0));
-    context = new SessionContext(session);
-    model = getTestModel(getRequiredTestModels().get(0));
-    project = model.getProject(session.getTransactionalEditingDomain());
-    structure = getStructure();
-    link = createLink();
-  }
-
-  @Override
-  public void test() throws Exception {
-    initialize();
-    preTest();
-    doTest();
+    return false;
   }
 
   protected void doTest() {
@@ -78,11 +50,5 @@ public abstract class CyberDiagramActivityExplorerTestCase extends CyberEmptyMod
 
     assertEquals(getNrOfDiagrams(), nrOfExistingDiagrams + 1);
     GuiActions.closeSession(session, false);
-
-  }
-
-  protected void preTest() {
-    ModelElement modelElement = getTestModelElement();
-    assertEquals(modelElement, structure);
   }
 }
