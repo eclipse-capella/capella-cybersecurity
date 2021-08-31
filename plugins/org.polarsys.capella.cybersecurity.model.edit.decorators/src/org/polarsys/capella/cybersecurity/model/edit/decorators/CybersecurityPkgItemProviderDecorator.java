e80fe02e8573ee03e34579f003d197036daf5b3e
Georgiana-Elena ECOBICI <georgiana-elena.ecobici@thalesgroup.com>
2021-08-31 11:09:54 +0300

    #38 Fix properties label on Primay Assets
     - properties label on Primay Assets: to be consistent for OA and other
    levels
    
    
    Change-Id: Ifff9e0245b4582e3ae450f18379ef5f979dd006d
    Signed-off-by: Georgiana-Elena ECOBICI <georgiana-elena.ecobici@thalesgroup.com>

--- plugins/org.polarsys.capella.cybersecurity.model.edit.decorators/src/org/polarsys/capella/cybersecurity/model/edit/decorators/CybersecurityPkgItemProviderDecorator.java
+++ plugins/org.polarsys.capella.cybersecurity.model.edit.decorators/src/org/polarsys/capella/cybersecurity/model/edit/decorators/CybersecurityPkgItemProviderDecorator.java
@@ -24,10 +24,15 @@ import org.eclipse.emf.edit.provider.IStructuredItemContentProvider;
 import org.eclipse.emf.edit.provider.ITreeItemContentProvider;
 import org.polarsys.capella.common.menu.dynamic.util.DynamicCommandParameter;
 import org.polarsys.capella.core.data.cs.BlockArchitecture;
+import org.polarsys.capella.core.data.ctx.CtxFactory;
+import org.polarsys.capella.core.data.epbs.EpbsFactory;
 import org.polarsys.capella.core.data.gen.edit.decorators.ItemProviderAdapterDecorator;
+import org.polarsys.capella.core.data.la.LaFactory;
 import org.polarsys.capella.core.data.oa.OaFactory;
 import org.polarsys.capella.core.data.oa.OperationalAnalysis;
+import org.polarsys.capella.core.data.pa.PaFactory;
 import org.polarsys.capella.core.model.helpers.BlockArchitectureExt;
+import org.polarsys.capella.core.model.helpers.BlockArchitectureExt.Type;
 import org.polarsys.capella.cybersecurity.model.CybersecurityFactory;
 import org.polarsys.capella.cybersecurity.model.CybersecurityPackage;
 import org.polarsys.capella.cybersecurity.model.CybersecurityPkg;
@@ -45,23 +50,56 @@ public class CybersecurityPkgItemProviderDecorator extends ItemProviderAdapterDe
   public Collection<?> getNewChildDescriptors(Object object, EditingDomain editingDomain, Object sibling) {
     Collection<Object> newChildDescriptors = (Collection<Object>) super.getNewChildDescriptors(object, editingDomain,sibling);
     BlockArchitecture architecture = BlockArchitectureExt.getRootBlockArchitecture((EObject) object);
-    if (architecture instanceof OperationalAnalysis) {
-      newChildDescriptors = newChildDescriptors.stream()
+    
+    newChildDescriptors = newChildDescriptors.stream()
         .filter(x -> x instanceof CommandParameter && !(((CommandParameter)x).getValue() instanceof FunctionalPrimaryAsset))
         .collect(Collectors.toList());
-      
-      OperationalAnalysis operationalAnalysis =  OaFactory.eINSTANCE.createOperationalAnalysis();
-      FunctionalPrimaryAsset primaryAsset = CybersecurityFactory.eINSTANCE.createFunctionalPrimaryAsset();
-      CybersecurityPkg pkg = CybersecurityFactory.eINSTANCE.createCybersecurityPkg();
-      
-      pkg.getOwnedPrimaryAssets().add(primaryAsset);
-      operationalAnalysis.getOwnedExtensions().add(pkg);
-      DynamicCommandParameter desc = new DynamicCommandParameter(null,
-          CybersecurityPackage.Literals.CYBERSECURITY_PKG__OWNED_PRIMARY_ASSETS,
-          primaryAsset, "Operational Activity Primary Asset");
-      
-      newChildDescriptors.add(desc);
+    
+    BlockArchitecture arch = createTmpArchitecture(architecture);
+    FunctionalPrimaryAsset primaryAsset = CybersecurityFactory.eINSTANCE.createFunctionalPrimaryAsset();
+    CybersecurityPkg pkg = CybersecurityFactory.eINSTANCE.createCybersecurityPkg();
+    
+    pkg.getOwnedPrimaryAssets().add(primaryAsset);
+    if(arch != null) {
+      arch.getOwnedExtensions().add(pkg);
     }
+    
+    String label = "Functional Primary Asset";
+    if (architecture instanceof OperationalAnalysis) {
+      label = "Operational Activity Primary Asset";
+    }
+    
+    DynamicCommandParameter desc = new DynamicCommandParameter(null,
+        CybersecurityPackage.Literals.CYBERSECURITY_PKG__OWNED_PRIMARY_ASSETS,
+        primaryAsset, label);
+    newChildDescriptors.add(desc);
+    
     return newChildDescriptors;
+  }
+  
+  private BlockArchitecture createTmpArchitecture(BlockArchitecture architecture) {
+    BlockArchitecture arch = null;
+    Type type = BlockArchitectureExt.getBlockArchitectureType(architecture);
+    switch(type) {
+    case OA:
+      arch = OaFactory.eINSTANCE.createOperationalAnalysis();
+      break;
+    case SA:
+      arch = CtxFactory.eINSTANCE.createSystemAnalysis();
+      break;
+    case LA:
+      arch = LaFactory.eINSTANCE.createLogicalArchitecture();
+      break;
+    case PA:
+      arch = PaFactory.eINSTANCE.createPhysicalArchitecture();
+      break;
+    case EPBS:
+      arch = EpbsFactory.eINSTANCE.createEPBSArchitecture();
+      break;  
+    default:
+      break;
+    }
+    
+    return arch;
   }
 }
