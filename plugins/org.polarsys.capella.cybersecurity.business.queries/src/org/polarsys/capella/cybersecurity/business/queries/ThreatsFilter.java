087b989351f4d0c7737ef2a1870344216fa325b9
Georgiana-Elena ECOBICI <georgiana-elena.ecobici@thalesgroup.com>
2021-10-27 12:20:26 +0300

    #130 Filter Threats on Capability/CapabilityRealization realized queries
    Change-Id: Id35bdc8ca4812bcd08a76c547c73b9be9af01ed7
    Signed-off-by: Georgiana-Elena ECOBICI
    <georgiana-elena.ecobici@thalesgroup.com>
    (cherry picked from commit e52f26849d0522192ff0af2714776ce8c3140a43)

---
+++ plugins/org.polarsys.capella.cybersecurity.business.queries/src/org/polarsys/capella/cybersecurity/business/queries/ThreatsFilter.java
@@ -0,0 +1,24 @@
+/*******************************************************************
+* Copyright © 2021 Thales Global Services SAS
+*
+* Licensed under the Thales Inner Source Software License:
+* Version 1.2, InnerOpen - Distribution Controlled
+*
+* You may not use this file except in compliance with the License.
+* You may obtain a copy of the License at https://gitlab.thalesdigital.io/Tiss-Organization/tiss-licenses
+* See the License for the specific language governing permissions and limitations under the License.
+*******************************************************************/
+package org.polarsys.capella.cybersecurity.business.queries;
+
+import org.polarsys.capella.common.queries.filters.IQueryFilter;
+import org.polarsys.capella.common.queries.queryContext.IQueryContext;
+import org.polarsys.capella.cybersecurity.model.Threat;
+
+
+public class ThreatsFilter implements IQueryFilter {
+
+  @Override
+  public boolean keepElement(Object element, IQueryContext context) {
+    return !(element instanceof Threat);
+  }
+}
