80f3da08a4a0b78a9ac165d01ecf836a006e806d
Georgiana-Elena ECOBICI <georgiana-elena.ecobici@thalesgroup.com>
2021-10-28 15:21:54 +0300

    #130 Add IS, FS with Cyber Layers in Activity explorer
    Change-Id: Iffa1f48ffe90a4df09ccbacf6798fbc232585f6a
    Signed-off-by: Georgiana-Elena ECOBICI
    <georgiana-elena.ecobici@thalesgroup.com>
    (cherry picked from commit 0433c259df42ec44947478aa6b40133b93ec1056)

---
+++ plugins/org.polarsys.capella.cybersecurity.ui/src/org/polarsys/capella/cybersecurity/ui/activity/CyberPANewFunctionalScenarioAdapter.java
@@ -0,0 +1,20 @@
+/*******************************************************************
+* Copyright © 2021 Thales Global Services SAS
+*
+* Licensed under the Thales Inner Source Software License:
+* Version 1.2, InnerOpen - Distribution Controlled
+*
+* You may not use this file except in compliance with the License.
+* You may obtain a copy of the License at https://gitlab.thalesdigital.io/Tiss-Organization/tiss-licenses
+* See the License for the specific language governing permissions and limitations under the License.
+*******************************************************************/
+package org.polarsys.capella.cybersecurity.ui.activity;
+
+import org.polarsys.capella.core.sirius.analysis.IDiagramNameConstants;
+
+public class CyberPANewFunctionalScenarioAdapter extends CyberPANewScenarioAdapter {
+  @Override
+  public String getRepresentationName() {
+    return IDiagramNameConstants.FUNCTIONAL_SCENARIO;
+  }
+}
