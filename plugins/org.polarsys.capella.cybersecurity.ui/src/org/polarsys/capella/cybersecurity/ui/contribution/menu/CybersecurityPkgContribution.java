cc5a45ef4b265e209e6fd1d2698770e6ce56fd19
Georgiana Ecobici <georgiana-elena.ecobici@thalesgroup.com>
2021-11-22 10:17:25 +0200

    #137 Manage the Cybersecurity inner packages
    Change-Id: I0f1681c460495b8966e8a096b51722529cec8c55
    Signed-off-by: Georgiana Ecobici <georgiana-elena.ecobici@thalesgroup.com>

---
+++ plugins/org.polarsys.capella.cybersecurity.ui/src/org/polarsys/capella/cybersecurity/ui/contribution/menu/CybersecurityPkgContribution.java
@@ -0,0 +1,51 @@
+/*******************************************************************
+* Copyright © 2021 Thales Global Services SAS
+*
+* Licensed under the Thales Inner Source Software License:
+* Version 1.2, InnerOpen - Distribution Controlled
+*
+* You may not use this file except in compliance with the License.
+* You may obtain a copy of the License at https://gitlab.thalesdigital.io/Tiss-Organization/tiss-licenses
+* See the License for the specific language governing permissions and limitations under the License.
+*******************************************************************/
+package org.polarsys.capella.cybersecurity.ui.contribution.menu;
+
+import org.eclipse.emf.common.command.Command;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.edit.domain.EditingDomain;
+import org.polarsys.capella.common.data.modellingcore.ModelElement;
+import org.polarsys.capella.common.menu.dynamic.contributions.IMDEMenuItemContribution;
+import org.polarsys.capella.core.data.cs.BlockArchitecture;
+import org.polarsys.capella.cybersecurity.model.CybersecurityPackage;
+import org.polarsys.capella.cybersecurity.sirius.analysis.CybersecurityServices;
+
+public class CybersecurityPkgContribution implements IMDEMenuItemContribution {
+
+  /**
+   * @see org.polarsys.capella.common.ui.menu.IMDEMenuItemContribution#executionContribution()
+   */
+  public Command executionContribution(EditingDomain editingDomain, ModelElement containerElement,
+      ModelElement createdElement, EStructuralFeature feature) {
+    return null;
+  }
+
+  /**
+   * @see org.polarsys.capella.common.ui.menu.IMDEMenuItemContribution#getMetaclass()
+   */
+  public EClass getMetaclass() {
+    return CybersecurityPackage.Literals.CYBERSECURITY_PKG;
+  }
+
+  /**
+   * @see org.polarsys.capella.common.ui.menu.IMDEMenuItemContribution#selectionContribution()
+   */
+  public boolean selectionContribution(ModelElement modelElement, EClass cls, EStructuralFeature feature) {
+    
+    if(modelElement instanceof BlockArchitecture) {
+      return new CybersecurityServices().getRootCibersecurityPkg((BlockArchitecture) modelElement) == null;
+    }
+    
+    return true;
+  }
+}
