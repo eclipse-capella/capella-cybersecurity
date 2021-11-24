8a8f696e814ec6277124259e12fbf371d23bba3a
MalinaStefaniaStoicanescu <malina.stoicanescu@thalesgroup.com>
2021-11-24 16:51:43 +0200

    #47 Add 'Primary Assets' section to Operational EPA Properties
    Change-Id: Id9fa4a1529e95385e9345083b78af51d3e8771e4
    Signed-off-by: MalinaStefaniaStoicanescu <malina.stoicanescu@thalesgroup.com>

---
+++ plugins/org.polarsys.capella.cybersecurity.ui/src/org/polarsys/capella/cybersecurity/ui/properties/sections/OperationalEnterprisePrimaryAssetSection.java
@@ -0,0 +1,26 @@
+/*******************************************************************
+* Copyright © 2021 Thales Global Services SAS
+*
+* Licensed under the Thales Inner Source Software License:
+* Version 1.2, InnerOpen - Distribution Controlled
+*
+* You may not use this file except in compliance with the License.
+* You may obtain a copy of the License at https://gitlab.thalesdigital.io/Tiss-Organization/tiss-licenses
+* See the License for the specific language governing permissions and limitations under the License.
+*******************************************************************/
+package org.polarsys.capella.cybersecurity.ui.properties.sections;
+
+import org.eclipse.emf.ecore.EObject;
+import org.polarsys.capella.cybersecurity.model.EnterprisePrimaryAsset;
+
+public class OperationalEnterprisePrimaryAssetSection extends EnterprisePrimaryAssetSection {
+  public OperationalEnterprisePrimaryAssetSection() {
+    super(false);
+  }
+  
+  @Override
+  public boolean select(Object toTest) {
+    EObject eObjectToTest = super.selection(toTest);
+    return eObjectToTest instanceof EnterprisePrimaryAsset && !isRealizedArchitecture(eObjectToTest);
+  }
+}
