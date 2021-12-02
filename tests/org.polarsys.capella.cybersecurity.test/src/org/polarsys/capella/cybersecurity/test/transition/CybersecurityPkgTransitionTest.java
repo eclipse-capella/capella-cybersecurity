5f32484fbe1072ba23e65b98c4b40f688dec3a10
Anita Matei Gabriel <matei.anita@thalesgroup.com>
2021-12-02 14:34:31 +0200

    #138 Fixed issue where cyber elements nested within multiple cyber pkgs weren't being transitioned
    Change-Id: I65c080f6cbd6c7f472caccbac53f4171d1e883fd
    Signed-off-by: Anita Matei Gabriel <matei.anita@thalesgroup.com>

--- tests/org.polarsys.capella.cybersecurity.test/src/org/polarsys/capella/cybersecurity/test/transition/CybersecurityPkgTransitionTest.java
+++ tests/org.polarsys.capella.cybersecurity.test/src/org/polarsys/capella/cybersecurity/test/transition/CybersecurityPkgTransitionTest.java
@@ -23,8 +23,14 @@ import org.polarsys.capella.cybersecurity.model.Threat;
 public class CybersecurityPkgTransitionTest extends CyberTopDownTransitionTestCase {
   Threat threat1;
   Threat threat2;
+  Threat threat3;
+  Threat threat4;
   PrimaryAsset primaryAsset1;
   PrimaryAsset primaryAsset2;
+  PrimaryAsset primaryAsset3;
+  PrimaryAsset primaryAsset4;
+  CybersecurityPkg cybersecPkg1;
+  CybersecurityPkg cybersecPkg2;
   PrimaryAssetMember primaryAssetMember;
   ExchangeItem exchangeItem;
 
@@ -34,18 +40,32 @@ public class CybersecurityPkgTransitionTest extends CyberTopDownTransitionTestCa
     // create threat
     threat1 = CybersecurityFactory.eINSTANCE.createThreat();
     threat2 = CybersecurityFactory.eINSTANCE.createThreat();
+    threat3 = CybersecurityFactory.eINSTANCE.createThreat();
+    threat4 = CybersecurityFactory.eINSTANCE.createThreat();
     primaryAsset1 = CybersecurityFactory.eINSTANCE.createFunctionalPrimaryAsset();
     primaryAsset2 = CybersecurityFactory.eINSTANCE.createInformationPrimaryAsset();
+    primaryAsset3 = CybersecurityFactory.eINSTANCE.createFunctionalPrimaryAsset();
+    primaryAsset4 = CybersecurityFactory.eINSTANCE.createInformationPrimaryAsset();
     primaryAssetMember = CybersecurityFactory.eINSTANCE.createPrimaryAssetMember();
     exchangeItem = InformationFactory.eINSTANCE.createExchangeItem();
     primaryAssetMember.setMember(exchangeItem);
     primaryAsset2.getOwnedMembers().add(primaryAssetMember);
+
+    // create 2 levels of nesting within the default cyber pkg, and a threat/pa within each new cyber pkg
+    cybersecPkg1 = CybersecurityFactory.eINSTANCE.createCybersecurityPkg();
+    cybersecPkg2 = CybersecurityFactory.eINSTANCE.createCybersecurityPkg();
+    cybersecPkg1.getOwnedThreats().add(threat3);
+    cybersecPkg1.getOwnedPrimaryAssets().add(primaryAsset3);
+    cybersecPkg2.getOwnedThreats().add(threat4);
+    cybersecPkg2.getOwnedPrimaryAssets().add(primaryAsset4);
 
     // add the threat on the operational level
     oaPkg.getOwnedThreats().add(threat1);
     oaPkg.getOwnedThreats().add(threat2);
     oaPkg.getOwnedPrimaryAssets().add(primaryAsset1);
     oaPkg.getOwnedPrimaryAssets().add(primaryAsset2);
+    oaPkg.getOwnedCybersecurityPkgs().add(cybersecPkg1);
+    cybersecPkg1.getOwnedCybersecurityPkgs().add(cybersecPkg2);
   }
 
   @Override
@@ -67,6 +87,14 @@ public class CybersecurityPkgTransitionTest extends CyberTopDownTransitionTestCa
     primaryAsset1 = (PrimaryAsset) mustBeTransitioned(primaryAsset1.getId(), pkg);
     primaryAsset2 = (PrimaryAsset) mustBeTransitioned(primaryAsset2.getId(), pkg);
     
+    // check if elements nested deeper in the cyber pkgs have been transitioned
+    cybersecPkg1 = (CybersecurityPkg) mustBeTransitioned(cybersecPkg1.getId(), pkg);
+    cybersecPkg2 = (CybersecurityPkg) mustBeTransitioned(cybersecPkg2.getId(), cybersecPkg1);
+    threat3 = (Threat) mustBeTransitioned(threat3.getId(), cybersecPkg1);
+    threat4 = (Threat) mustBeTransitioned(threat4.getId(), cybersecPkg2);
+    primaryAsset3 = (PrimaryAsset) mustBeTransitioned(primaryAsset3.getId(), cybersecPkg1);
+    primaryAsset4 = (PrimaryAsset) mustBeTransitioned(primaryAsset4.getId(), cybersecPkg2);
+
     BlockArchitecture blockArchitecture = BlockArchitectureExt.getRootBlockArchitecture(pkg);
     long noPkg = blockArchitecture.getOwnedExtensions().stream().filter(CybersecurityPkg.class::isInstance).count();
     // only one CybersecurityPackage shall be transitioned
