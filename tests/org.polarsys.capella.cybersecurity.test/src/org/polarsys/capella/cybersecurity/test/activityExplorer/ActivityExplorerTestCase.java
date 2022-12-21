6d1f9dc423a71161426e2457a670680450bb0f2d
Erwann Traisnel <erwann.traisnel@obeo.fr>
2022-12-21 17:27:22 +0100

    #92 Changed licence to EPL 2.0
    Added licence where it was missing
    
    Signed-off-by: Erwann Traisnel <erwann.traisnel@obeo.fr>
    Change-Id: I0e7d24f2186ccb22791bbe76eeeb141307381e6d
    Signed-off-by: Philippe DUL <philippe.dul@thalesgroup.com>

--- tests/org.polarsys.capella.cybersecurity.test/src/org/polarsys/capella/cybersecurity/test/activityExplorer/ActivityExplorerTestCase.java
+++ tests/org.polarsys.capella.cybersecurity.test/src/org/polarsys/capella/cybersecurity/test/activityExplorer/ActivityExplorerTestCase.java
@@ -1,4 +1,17 @@
+/*******************************************************************************
+ * Copyright (c) 2021 THALES GLOBAL SERVICES.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *   
+ * Contributors:
+ *    Thales - initial API and implementation
+ *******************************************************************************/
 package org.polarsys.capella.cybersecurity.test.activityExplorer;
+
 import org.eclipse.amalgam.explorer.activity.ui.api.hyperlinkadapter.AbstractHyperlinkAdapter;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.sirius.business.api.session.Session;
