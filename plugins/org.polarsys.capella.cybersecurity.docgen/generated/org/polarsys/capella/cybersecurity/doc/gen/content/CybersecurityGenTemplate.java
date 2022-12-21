7258d447c37b61ad903cebf91adf764714c8c466
Erwann Traisnel <erwann.traisnel@obeo.fr>
2022-12-21 17:27:22 +0100

    #94 Changed licence to EPL 2.0
    Added licence where it was missing
    
    Change-Id: I0e7d24f2186ccb22791bbe76eeeb141307381e6d
    Signed-off-by: Erwann Traisnel <erwann.traisnel@obeo.fr>
    Signed-off-by: Philippe DUL <philippe.dul@thalesgroup.com>

--- plugins/org.polarsys.capella.cybersecurity.docgen/generated/org/polarsys/capella/cybersecurity/doc/gen/content/CybersecurityGenTemplate.java
+++ plugins/org.polarsys.capella.cybersecurity.docgen/generated/org/polarsys/capella/cybersecurity/doc/gen/content/CybersecurityGenTemplate.java
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2021 THALES GLOBAL SERVICES.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *   
+ * Contributors:
+ *    Thales - initial API and implementation
+ *******************************************************************************/
 //Generated with EGF 1.6.2.202001031546
 package org.polarsys.capella.cybersecurity.doc.gen.content;
 
